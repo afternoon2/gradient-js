2e1166b627bd574433520f77807d35f135a6c069
afternoon2 <poprostuantolak@gmail.com>
2019-10-05 15:36:16 +0200

    feat(core): add core module functionality

--- packages/core/__tests__/core.test.ts
+++ packages/core/__tests__/core.test.ts
@@ -1,5 +1,6 @@
 import { Core, CoreOptions } from '../lib/core';
 import { messages } from '../lib/messages';
+import { Color } from 'chroma-js';
 
 describe('@gradient-js/core', () => {
   describe('Core output generation', () => {
@@ -16,6 +17,74 @@ describe('@gradient-js/core', () => {
         core.get(fakeColors, config);
       };
       expect(exec).toThrowError(messages.mixedColorTypes);
+    });
+
+    test("If it generates chroma colors array with correct length", () => {
+      const colors: string[] = ['rgb(10, 20, 30)', '#ff3399', 'rgba(34, 134, 255, 0.9)'];
+      const config: CoreOptions = {
+        useBezier: true,
+        interpolation: 'hsl',
+        samples: 20,
+        lightnessCorrection: true,
+      };
+      const core = new Core();
+      const result: Color[] = core.get(colors, config);
+      expect(result.length).toBe(config.samples);
+    });
+
+    test('If all colors are of the same type', () => {
+      const colors: string[] = ['rgb(10, 20, 30)', '#ff3399', 'rgba(34, 134, 255, 0.9)'];
+      const config: CoreOptions = {
+        useBezier: true,
+        interpolation: 'hsl',
+        samples: 20,
+        lightnessCorrection: true,
+      };
+      const core = new Core();
+      const result: Color[] = core.get(colors, config);
+      // @ts-ignore
+      expect(result.find((color: Color) => !Array.isArray(color['_rgb']))).toBe(undefined);
+    });
+
+    test('If it generates an output with linear scale correctly', () => {
+      const colors: string[] = ['rgb(10, 20, 30)', '#ff3399', 'rgba(34, 134, 255, 0.9)'];
+      const config: CoreOptions = {
+        useBezier: false,
+        interpolation: 'hsl',
+        samples: 20,
+        lightnessCorrection: true,
+      };
+      const core = new Core();
+      const result: Color[] = core.get(colors, config);
+      // @ts-ignore
+      expect(result.find((color: Color) => !Array.isArray(color['_rgb']))).toBe(undefined);
+    });
+
+    test('If it generates an output with linear scale and no lightness correction correctly', () => {
+      const colors: string[] = ['rgb(10, 20, 30)', '#ff3399', 'rgba(34, 134, 255, 0.9)'];
+      const config: CoreOptions = {
+        useBezier: false,
+        interpolation: 'hsl',
+        samples: 20,
+        lightnessCorrection: false,
+      };
+      const core = new Core();
+      const result: Color[] = core.get(colors, config);
+      expect(result.find((c: Color) => !c.hasOwnProperty('_rgb'))).toBe(undefined);
+    });
+
+    test('If it generates an output without interpolation', () => {
+      const colors: string[] = ['rgb(10, 20, 30)', '#ff3399', 'rgba(34, 134, 255, 0.9)'];
+      const config: CoreOptions = {
+        useBezier: false,
+        // @ts-ignore
+        interpolation: null,
+        samples: 20,
+        lightnessCorrection: false,
+      };
+      const core = new Core();
+      const result: Color[] = core.get(colors, config);
+      expect(result.find((c: Color) => !c.hasOwnProperty('_rgb'))).toBe(undefined);
     });
   });
 });
--- packages/core/lib/core.ts
+++ packages/core/lib/core.ts
@@ -1,5 +1,64 @@
+import chroma, { Color, Scale, InterpolationMode } from 'chroma-js';
+import { messages } from './messages';
+
+export type CoreOptions = {
+  useBezier: boolean;
+  interpolation: InterpolationMode;
+  samples: number;
+  lightnessCorrection: boolean;
+};
+
+type ScaleResult = {
+  scale: Scale<Color>;
+  options: CoreOptions;
+};
+
 export class Core {
-  get value(): string {
-    return 'core';
+  private defaultOptions: CoreOptions = {
+    useBezier: false,
+    interpolation: 'rgb',
+    samples: 10,
+    lightnessCorrection: false,
+  };
+
+  get(colors: string[], options: CoreOptions = this.defaultOptions): Color[] {
+    if (colors.find((val: unknown) => typeof val !== 'string')) {
+      throw new Error(messages.mixedColorTypes);
+    }
+    const { getLinearScale, getBezierScale, getBase } = this;
+    const scale = options.useBezier ? getBezierScale : getLinearScale;
+    return getBase(scale(options, colors));
+  }
+
+  private getLinearScale(options: CoreOptions, colors: string[]): ScaleResult {
+    let result: ScaleResult;
+    const scaled = chroma.scale(colors);
+    if (!options.interpolation) {
+      const scaledWithInterp = scaled;
+      result = {
+        scale: options.lightnessCorrection ? scaledWithInterp.correctLightness() : scaledWithInterp,
+        options,
+      };
+    } else {
+      result = {
+        scale: options.lightnessCorrection ? scaled.mode(options.interpolation).correctLightness() : scaled,
+        options,
+      };
+    }
+    return result;
+  }
+
+  private getBezierScale(options: CoreOptions, colors: string[]): ScaleResult {
+    const beziered = chroma.bezier(colors).scale();
+    return {
+      scale: options.lightnessCorrection ? beziered.correctLightness() : beziered,
+      options,
+    };
+  }
+
+  private getBase(result: ScaleResult): Color[] {
+    return new Array(result.options.samples)
+      .fill('')
+      .map((el: string, index: number) => result.scale(index / result.options.samples));
   }
 }
