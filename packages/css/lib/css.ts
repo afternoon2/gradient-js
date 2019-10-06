dbf6fb08b99dfb8762cb9a51588bc08d5e3d861a
afternoon2 <poprostuantolak@gmail.com>
2019-10-06 12:21:39 +0200

    style(css): run lint fix

--- packages/css/lib/css.ts
+++ packages/css/lib/css.ts
@@ -2,12 +2,12 @@ import { Core, CoreOptions } from '@gradient-js/core';
 import { Color } from 'chroma-js';
 
 export type CssOptions = CoreOptions & {
-  type: 'linear' | 'radial',
-  angle?: number,
-  left?: number,
-  top?: number,
-  shape?: 'ellipse' | 'circle',
-  extentKeyword?: 'none' | 'closest-side' | 'closest-corner' | 'farthest-side' | 'farthest-corner',
+  type: 'linear' | 'radial';
+  angle?: number;
+  left?: number;
+  top?: number;
+  shape?: 'ellipse' | 'circle';
+  extentKeyword?: 'none' | 'closest-side' | 'closest-corner' | 'farthest-side' | 'farthest-corner';
 };
 export class Css {
   private core: Core;
@@ -18,7 +18,7 @@ export class Css {
     this.core = new Core();
   }
 
-  get(colors: string[], options: CssOptions) {
+  get(colors: string[], options: CssOptions): string {
     this.options = options;
     this.colors = this.core.get(colors, options);
     return `${options.type}-gradient(${this.angle}${this.shape}${this.extentKeyword}${this.cssifyColors()})`;
@@ -31,27 +31,17 @@ export class Css {
 
   private get angle(): string {
     const options = this.options as CssOptions;
-    return options.angle && options.type === 'linear'
-      ? `${options.angle}deg, `
-      : '';
+    return options.angle && options.type === 'linear' ? `${options.angle}deg, ` : '';
   }
 
   private get shape(): string {
     const options = this.options as CssOptions;
-    let shape: string = '';
+    let shape = '';
     if (options.type === 'radial') {
-      if (
-        options.shape &&
-        options.shape !== 'ellipse' &&
-        (!options.top && !options.left)
-      ) {
+      if (options.shape && options.shape !== 'ellipse' && (!options.top && !options.left)) {
         shape = `${options.shape}, `;
       }
-      if (
-        options.shape &&
-        options.shape !== 'ellipse' &&
-        (options.top && options.left)
-      ) {
+      if (options.shape && options.shape !== 'ellipse' && (options.top && options.left)) {
         shape = `${options.shape} at ${options.left}% ${options.top}%, `;
       }
       if (
@@ -79,21 +69,10 @@ export class Css {
   private get extentKeyword(): string {
     let extentKeyword: string;
     const options = this.options as CssOptions;
-    const afterExtent = options.top && options.left
-      ? ` at ${options.left}% ${options.top}%, `
-      : ', ';
-    if (
-      options.shape === 'ellipse' &&
-      options.extentKeyword &&
-      options.extentKeyword !== 'none'
-    ) {
+    const afterExtent = options.top && options.left ? ` at ${options.left}% ${options.top}%, ` : ', ';
+    if (options.shape === 'ellipse' && options.extentKeyword && options.extentKeyword !== 'none') {
       extentKeyword = options.extentKeyword + afterExtent;
-    } else if (
-      options.shape === 'ellipse' &&
-      !options.extentKeyword &&
-      options.top &&
-      options.left
-    ) {
+    } else if (options.shape === 'ellipse' && !options.extentKeyword && options.top && options.left) {
       extentKeyword = afterExtent;
     } else {
       extentKeyword = '';
